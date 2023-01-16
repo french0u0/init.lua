41a1fd7b8aedb8b731f3307b3abf8e20b3084f9a
Jacobs, Frans (INT) <frans.jacobs@teledyne.com>
2023-01-16 14:56:47 +0100

    latest changes

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>ga", ":G add -vu<CR>")
+vim.keymap.set("n", "<leader>gc", ":G commit -m \"")
+vim.keymap.set("n", "<leader>gp", ":G push<CR>")
